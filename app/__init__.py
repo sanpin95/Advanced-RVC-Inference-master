ce470e4e858bb6d3eb4a77853afbd7e14e91c7f6
san <ultimatervcsan@gmail.com>
2026-07-21 11:13:51 +0530

    Delete app directory

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-# App Module
-# Main application entry points (GUI, CLI, etc.)
-
-from .gui import launch as launch_gui
-
-
-
-__all__ = ['launch_gui']
--- app/tabs/downloads/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Downloads tab for Advanced RVC Inference."""
-from .downloads import download_tab
-
-__all__ = ["download_tab"]
